00ce4459148f5e23eb11860cd911cd851538971e
Manuel Marín <mawitu@gmail.com>
2013-01-19 10:33:18 +0100

    Adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laya::Application.config.secret_token = 'b64a7c455709fd32f214925303d7e0c2b256e9aa4a6c7d57fd621a8ebd072ceed9d575b1bf49d261d1811373fff6300075e0fe6e28418748897ad94716a036e3'
