78c8509fe558baed597f23e19c46ff3524fec716
Manuel Marín <mawitu@gmail.com>
2013-01-23 20:02:20 +0100

    Minor fixes

--- config/initializers/init_laya_plugins_data.rb
+++ config/initializers/init_laya_plugins_data.rb
@@ -1,6 +1,13 @@
 laya_gems = Gem.loaded_specs.select{|x,y| x.include?("laya_")}
 
+
 laya_gems.each do |x,y|
-  klass = eval(x.camelize)
-  klass::Config.init_data
+  begin
+    klass = eval(x.camelize)
+    klass::Config.init_data
+  rescue Exception => e
+     p "Failed to load #{x} plugin"
+  end
+
+
 end
